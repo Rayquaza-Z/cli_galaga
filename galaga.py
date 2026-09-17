25e6bd7bd2e051ac0a8f064f78b6cec2a87ee8cb
Aayaan Singh <aayaansingh@Administrators-MacBook-Air-4.local>
2026-09-18 02:38:09 +0530

    bullets

--- galaga.py
+++ galaga.py
@@ -16,6 +16,16 @@ def main(stdscr):
     x = width // 2
     y = height // 2
 
+
+#==================================
+    bullets=[]
+
+
+
+
+#==================================
+
+
     while True:
         key = stdscr.getch()
         if key == ord('q'):
@@ -24,12 +34,21 @@ def main(stdscr):
             x = max(0,x-4)
         elif key== curses.KEY_RIGHT:
             x = min(width-1 , x+4)
+        elif key== ord(' '):
+            bullets.append([x,y+y//2-1])
+        for b in bullets:
+            b[1]-=1
+        bullets = [b for b in bullets if b[1]>0]
+
+
         stdscr.erase()
         stdscr.addstr(y+y//2, x, "A")
+
+        for b in bullets:
+            stdscr.addstr(b[1], b[0], "#")
         stdscr.refresh()
 
 curses.wrapper(main)
-
 
 #--------------------------
 #
