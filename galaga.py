1864a68c75a6a93600c24b9b9fdbe5c525a3b5f2
Aayaan Singh <aayaansingh@Administrators-MacBook-Air-4.local>
2026-09-18 13:17:04 +0530

    Game over and restart

--- galaga.py
+++ galaga.py
@@ -12,63 +12,83 @@ def main(stdscr):
     stdscr.nodelay(True)
     stdscr.timeout(50)
 
+
     height, width = stdscr.getmaxyx()
-    x = width // 2
-    y = height // 2
+
+    def new_game():
+        x = width // 2
+        y = height // 2
 
 
-#==================================
-    bullets=[]
-    enemies=[]
-    e_bullets=[]
-    '''if random.random() <0.8:
-        shooter = random.choice(enemies)
-        e_bullets.append([shooter[0], shooter[1]+1])'''
+
+        bullets=[]
+        enemies=[]
+        e_bullets=[]
 
     
 
-    enemy_count=9
-    enemy_space=4
-    center_x=width//2
-    middle_ind = enemy_count//2
+        enemy_count=9
+        enemy_space=4
+        center_x=width//2
+        middle_ind = enemy_count//2
 
 
-    for i in range(enemy_count):
-        offset= (i-middle_ind)*enemy_space
-        enemies.append([center_x + offset,2])
+        for i in range(enemy_count):
+            offset= (i-middle_ind)*enemy_space
+            enemies.append([center_x + offset,2])
+
+        lives=3
+        game_over=False
 
 
+        return x,y,bullets,e_bullets,enemies , lives , game_over
 
+    x, y, bullets, e_bullets, enemies, lives, game_over = new_game()
 
     while True:
         key = stdscr.getch()
         if key == ord('q'):
             break
+        if game_over and key == ord('r'):
+            x, y, bullets, e_bullets, enemies, lives, game_over = new_game()
         elif key== curses.KEY_LEFT:
             x = max(0,x-4)
         elif key== curses.KEY_RIGHT:
             x = min(width-1 , x+4)
         elif key== ord(' '):
             bullets.append([x,y+y//2-1])
-        for b in bullets:
-            b[1]-=1
-        bullets = [b for b in bullets if b[1]>0]
 
-        for eb in e_bullets:
-            eb[1]+=1
-        e_bullets=[eb for eb in e_bullets if eb[1]<height-1] 
+        if not game_over:
+            
 
 
-        for b in bullets[:]:
-            for e in enemies[:]:
-                if b[0]==e[0] and b[1]==e[1]:
-                    bullets.remove(b)
-                    enemies.remove(e)
+            for b in bullets:
+                b[1]-=1
+            bullets = [b for b in bullets if b[1]>0]
+
+            for eb in e_bullets:
+                eb[1]+=1
+            e_bullets=[eb for eb in e_bullets if eb[1]<height-1] 
+
+
+            for b in bullets[:]:
+                for e in enemies[:]:
+                    if b[0]==e[0] and b[1]==e[1]:
+                        bullets.remove(b)
+                        enemies.remove(e)
+                        break
+
+            for eb in e_bullets[:]:
+                if eb[1]==y+y//2 and eb[0]==x:
+                    lives-=1
+                    e_bullets.remove(eb)
+                    if lives==0:
+                        game_over=True
                     break
 
-        if enemies and random.random()<0.08:
-            shooter=random.choice(enemies)
-            e_bullets.append([shooter[0],shooter[1]+1])
+            if enemies and random.random()<0.08:
+                shooter=random.choice(enemies)
+                e_bullets.append([shooter[0],shooter[1]+1])
 
 
 
@@ -84,6 +104,9 @@ def main(stdscr):
             stdscr.addstr(e[1],e[0],"W")
         for eb in e_bullets:
             stdscr.addstr(eb[1],eb[0],"!")
+
+        if game_over:
+            stdscr.addstr(height//2, width//2 - 5 , "GAME OVER")
 
         stdscr.refresh()
 
