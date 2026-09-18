9967f15384df6d2ac00df1d8cc32e314df733b5e
Aayaan Singh <aayaansingh@Administrators-MacBook-Air-4.local>
2026-09-18 11:39:20 +0530

    Enemy Collision

--- galaga.py
+++ galaga.py
@@ -19,11 +19,21 @@ def main(stdscr):
 
 #==================================
     bullets=[]
+    enemies=[]
+
+    enemy_count=9
+    enemy_space=4
+    center_x=width//2
+    middle_ind = enemy_count//2
 
 
+    for i in range(enemy_count):
+        offset= (i-middle_ind)*enemy_space
+        enemies.append([center_x + offset,2])
 
+    '''for col in range(8):
+        enemies.append([8 + col * 4, 2])'''
 
-#==================================
 
 
     while True:
@@ -41,11 +51,21 @@ def main(stdscr):
         bullets = [b for b in bullets if b[1]>0]
 
 
+        for b in bullets:
+            for e in enemies:
+                if b[0]==e[0] and b[1]==e[1]:
+                    bullets.remove(b)
+                    enemies.remove(e)
+                    break
+
         stdscr.erase()
         stdscr.addstr(y+y//2, x, "A")
 
         for b in bullets:
             stdscr.addstr(b[1], b[0], "#")
+
+        for e in enemies:
+            stdscr.addstr(e[1],e[0],"W")
         stdscr.refresh()
 
 curses.wrapper(main)
